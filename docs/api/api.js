1ef03f88aa2ad639b1429e5bdcb791c64117f929
Josh Loehr <jloehr@spredfast.com>
2015-08-26 17:04:23 -0500

    Added documentation

---
+++ docs/api/api.js
@@ -0,0 +1,23 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Connection",
+        "ContentError",
+        "Message",
+        "OAuth",
+        "OptionError",
+        "Util"
+    ],
+    "modules": [
+        "Spredfast"
+    ],
+    "allModules": [
+        {
+            "displayName": "Spredfast",
+            "name": "Spredfast",
+            "description": "Provides the base Spredfast API module."
+        }
+    ],
+    "elements": []
+} };
+});
